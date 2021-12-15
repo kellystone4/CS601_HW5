d34c9987a6c010ba9a3dd06596ce876087f307c6
Kelly Stone <kellystone916@gmail.com>
2021-12-15 15:29:11 -0800

    add innerhtml

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ async function getDegrees(url) {
     //get your data here, and check for the response status. If it's not 200, throw an error
     .then((response) => response.json())
     .then((data) =>
-      document.write(
+      document.innerHTML(
                            <div id="result"> ` 
                             My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
                             and my second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}.`
