a3042e88e7b55467ed8f921a5ef107c0a6b80f97
Kelly Stone <kellystone916@gmail.com>
2021-12-15 15:29:58 -0800

    add innerhtml

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ async function getDegrees(url) {
     .then((response) => response.json())
     .then((data) =>
       document.innerHTML(
-                           <div id="result"> ` 
-                            My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
+                            `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
                             and my second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}.`
-                            </div>
       )
     )
 }
