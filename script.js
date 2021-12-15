caf02e8a1c0fe69b06c1473c5ad5f8a4d73c2c2e
Kelly Stone <kellystone916@gmail.com>
2021-12-15 15:41:22 -0800

    revert

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let inhtml = document.getElementById('result')
+// let inhtml = document.getElementById('result')
 
 function clickBtn() {
 async function getDegrees(url) {
-  // fetch the url
   await fetch(url)
-    //get your data here, and check for the response status. If it's not 200, throw an error
     .then((response) => response.json())
     .then((data) =>
-      document.getElementById("result").write(
+      document.write(
                             `My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
                             and my second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}.`
+
       )
     )
 }
