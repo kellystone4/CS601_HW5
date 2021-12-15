f06325c29ae3a3d1ec558875346a345276bf8148
Kelly Stone <kellystone916@gmail.com>
2021-12-15 15:24:28 -0800

    testBtn

--- script.js
+++ script.js
@@ -114,22 +114,23 @@
 
 // var url = "https://kellystone4.github.io/CS601_HW5_Stone/"
 
-// // if (onclick.clickBtn) {
-async function getDegrees(url) {
-    // fetch the url
-    await fetch(url)
-      //get your data here, and check for the response status. If it's not 200, throw an error
-      .then((response) => response.json())
-      .then((data) =>
-        document.write(
-                              `Hello, welcome to my site! Learn about my degree's here: 
-                              My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
-                              and my second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}.`
-                
-        )
-      )
-  }
 
-  getDegrees("./degrees.json")
+function clickBtn() {
+async function getDegrees(url) {
+  // fetch the url
+  await fetch(url)
+    //get your data here, and check for the response status. If it's not 200, throw an error
+    .then((response) => response.json())
+    .then((data) =>
+      document.write(
+                            `Hello, welcome to my site! Learn about my degree's here: 
+                            My first degree was a ${data.data[0].degrees.bachelors.data.type} from ${data.data[0].degrees.bachelors.data.school} in ${data.data[0].degrees.bachelors.data.program}
+                            and my second degree was a ${data.data[0].degrees.masters.data.type} from ${data.data[0].degrees.masters.data.school} in ${data.data[0].degrees.masters.data.program}.`
+              
+      )
+    )
+}
+
+getDegrees("./degrees.json")}
 
 
